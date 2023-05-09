0e1424091a73a1a614a0cd87a3c678728e7dfd42
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-05-09 13:03:14 +0900

    login function

--- graphql_api/graphql_api/settings.py
+++ graphql_api/graphql_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'api.apps.ApiConfig',
+    # 'graphql_api.apps.ApiConfig',
     'users.apps.UsersConfig',
     'corsheaders',
 ]
